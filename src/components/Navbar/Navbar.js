d730b4d7ccb541c06a696718f97665aaf615bf2c
Brett Huber <bretthuber6@gmail.com>
2020-01-27 22:07:46 -0600

    add bulma styling and basic navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="https://bulma.io">
+                    Capacity Blog
+                </a>   
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
